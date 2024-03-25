77fc628601ede355197eb4f63d7c721f08accc06
Josh Gitter <jjgitter@hotmail.com>
2024-03-25 15:07:47 +0900

    whatever

---
+++ Scrabble/scrabble.py
@@ -0,0 +1,36 @@
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+points = [1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 4, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10]
+
+letter_to_points = {letter: point for letter, point in zip(letters, points)}
+
+def score_word(word):
+  point_total = 0
+  for letter in word:
+    if letter in letter_to_points:
+      point_total += letter_to_points[letter]
+    else:
+      point_total += 0
+  return point_total
+
+brownie_points = score_word("BROWNIE")
+print(brownie_points)
+
+player_to_words = {}
+player_to_words["player1"] = ["BLUE", "TENNIS", "EXIT"]
+player_to_words["wordNerd"] = ["EARTH", 
+"EYES", "MACHINE"]
+player_to_words["Lexi Con"] = ["ERASER", 
+"BELLY", "HUSKY"]
+player_to_words["Prof Reader"] = ["ZAP", 
+"COMA", "PERIOD"]
+
+player_to_points = {}
+player_points = 0
+
+for player, words in player_to_words.items():
+ player_points = 0
+ for word in words:
+    player_points += score_word(word)
+    player_to_points[player] = player_points
+
+print(player_to_points)
